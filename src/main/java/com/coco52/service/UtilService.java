408ce8e9ca75eeee2b8a9270e53f1946ccffaea0
filk <2470982985@qq.com>
2021-09-07 23:21:16 +0800

    added a check in function

--- src/main/java/com/coco52/service/UtilService.java
+++ src/main/java/com/coco52/service/UtilService.java
@@ -2,6 +2,8 @@ package com.coco52.service;
 
 import com.coco52.entity.RespMsg;
 
+import javax.servlet.http.HttpServletRequest;
+
 public interface UtilService {
-    RespMsg sign(String url,String userId);
+    RespMsg sign(String url, String userId, HttpServletRequest request);
 }
--- src/main/java/com/coco52/service/impl/UtilServiceImpl.java
+++ src/main/java/com/coco52/service/impl/UtilServiceImpl.java
@@ -1,9 +1,14 @@
 package com.coco52.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
+import com.coco52.entity.AccessLog;
 import com.coco52.entity.RespMsg;
+import com.coco52.mapper.AccessLogMapper;
 import com.coco52.service.UtilService;
 import com.coco52.util.MyUtils;
+import eu.bitwalker.useragentutils.Browser;
+import eu.bitwalker.useragentutils.OperatingSystem;
+import eu.bitwalker.useragentutils.UserAgent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -14,9 +19,11 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 import org.thymeleaf.util.DateUtils;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -29,10 +36,12 @@ public class UtilServiceImpl implements UtilService {
     private RestTemplate restTemplate;
 
     @Autowired
+    private AccessLogMapper accessLogMapper;
+    @Autowired
     private MyUtils myUtils;
 
     @Override
-    public RespMsg sign(String url, String userId) {
+    public RespMsg sign(String url, String userId, HttpServletRequest request) {
         Date parseDate = null;
         Date date = new Date();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
@@ -43,18 +52,17 @@ public class UtilServiceImpl implements UtilService {
         Map<String, String> urlParam = myUtils.getUrlParam(url);
         String e = urlParam.get("e");
 
-        String text = "jparam={'empids': '" + userId + "', 'loctime': '" + sdf.format(date) + "', 'coordinate': '113.94837060116612,22.558772759333863', 'Location': '广东省深圳市南山区新西路9-南门', 'photoid': '', 'description': ''}";
+        String text = "jparam={'empids': '" + userId + "', 'loctime': '" + sdf.format(date) + "', 'coordinate': '113.94837060116612,22.558772759333863', 'Location': '', 'photoid': '', 'description': ''}";
         String s = null;
         try {
-            s = new String(text.getBytes(), "utf-8");
+            s = new String(text.getBytes("UTF-8"));
         } catch (UnsupportedEncodingException unsupportedEncodingException) {
             return RespMsg.fail("错误，未知异常,请联系管理员处理", unsupportedEncodingException.getCause());
         }
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.set("Content-Type", "application/x-www-form-urlencoded");
         headers.set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36");
-        HttpEntity<String> entity = new HttpEntity<String>(s, headers);
-
+        HttpEntity<String> entity = new HttpEntity<>(s, headers);
         ResponseEntity<String> forEntity = restTemplate.postForEntity("http://ehr.yadi-group.com:82/YDHR/m.aspx?e=" + e + "&s=AttLbsSign.SQL", entity, String.class);
         int statusCode = forEntity.getStatusCodeValue();
         if (statusCode != 200 || forEntity.getHeaders().containsKey("Location")) {
@@ -70,8 +78,28 @@ public class UtilServiceImpl implements UtilService {
             return RespMsg.fail("Error,网站返回解析异常", forEntity.getBody());
         }
         long l = date.getTime() - parseDate.getTime();
-        boolean b = Math.abs(l) < 1000 * 60 * 10;
-        System.out.println(l);
+        boolean b = Math.abs(l) > 0;
+        String agent = request.getHeader("User-Agent");
+        UserAgent userAgent = UserAgent.parseUserAgentString(agent);
+        Browser browser = userAgent.getBrowser();
+        OperatingSystem os = userAgent.getOperatingSystem();
+
+        AccessLog accessLog = new AccessLog(
+                null,
+                browser.getName(),
+                browser.getBrowserType().getName(),
+                browser.getGroup().toString(),
+                browser.getManufacturer().toString(),
+                browser.getRenderingEngine().toString(),
+                userAgent.getBrowserVersion().toString(),
+                os.getName(),
+                os.getGroup().toString(),
+                os.getManufacturer().toString(),
+                os.getDeviceType().toString(),
+                request.getRemoteAddr(),
+                new Timestamp(new Date().getTime())
+        );
+        accessLogMapper.insert(accessLog);
         return b ? RespMsg.success("打卡成功！", forEntity.getBody()) : RespMsg.fail("Error，签到失败,参数可能填写错误！", forEntity.getBody());
     }
 }
