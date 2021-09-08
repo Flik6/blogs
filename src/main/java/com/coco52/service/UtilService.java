5de3c94c73b9be12992d4fe2eab2e0ad44864c1c
flik <2470982985@qq.com>
2021-09-08 18:19:07 +0800

    Minor edits

--- src/main/java/com/coco52/service/UtilService.java
+++ src/main/java/com/coco52/service/UtilService.java
@@ -3,7 +3,9 @@ package com.coco52.service;
 import com.coco52.entity.MyUser;
 import com.coco52.entity.RespMsg;
 
+import javax.servlet.http.HttpServletRequest;
+
 public interface UtilService {
-    RespMsg sign(String url,String userId);
+    RespMsg sign(String url, String userId, HttpServletRequest request);
 
 }
